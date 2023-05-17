36ca4af26dd6cbb7e8a6fae934927b3973dc4557
aissaGbibar <gbibaraissa@gmail.com>
2023-05-17 11:07:49 +0100

    Change form name

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 }); 
-Route::get("/form",function(){ return view("form");});
+Route::get("/form2",function(){ return view("form");});
